02867e1b784cfb6ce7c9ec91101812b81ec09f6d
Ayodele <daylay92@yahoo.com>
2020-09-16 09:49:31 +0100

    add missing microservice option

--- microservices/order-service/src/order.module.ts
+++ microservices/order-service/src/order.module.ts
@@ -4,13 +4,13 @@ import { OrderService } from './order.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Order, OrderSchema } from './order.schema';
 import { ClientsModule } from '@nestjs/microservices';
-import { carServiceOptions, walletServiceOptions, userServiceOptions } from './config/grpc.options';
+import { carServiceOptions, walletServiceOptions, userServiceOptions, emailServiceOptions } from './config/grpc.options';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/env';
 
 @Module({
   imports: [
-    ClientsModule.register([carServiceOptions, walletServiceOptions, userServiceOptions]),
+    ClientsModule.register([carServiceOptions, walletServiceOptions, userServiceOptions, emailServiceOptions ]),
     ConfigModule.forRoot({
       load: [configuration],
     }),
